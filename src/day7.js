010c6f9d250bf74a2ca8f65b91493054560a1f47
dwi2 <dwi2@dwi2.com>
2022-12-07 15:24:07 +0900

    feat: Day 7 dirty solution

---
+++ src/day7.js
@@ -0,0 +1,62 @@
+const commands = [];
+const output = [];
+let buffer = [];
+let dirStack = [];
+
+module.exports = {
+  reading: (line) => {
+    if (line.length > 0) {
+      if (line.startsWith('$')) {
+        if (buffer.length > 0) {
+          output.push(buffer);
+          buffer = [];
+        }
+        commands.push(line.substring(2));
+      } else {
+        buffer.push(line);
+      }  
+    }
+  },
+  solving: () => {
+    output.push(buffer);
+    buffer = [];    
+
+    const dirMap = new Map();
+    let size = 0;
+    commands.forEach(command => {
+      if (command.startsWith('cd')) {
+        const dirName = command.substring(3);
+        if (dirName === '..') {
+          dirStack.pop();
+          const cwd = dirStack.join("/");
+          if (dirMap.has(cwd)) {
+            const oldSize = dirMap.get(cwd);
+            size = oldSize + size;
+            dirMap.set(cwd, size);
+          }
+        } else {
+          dirStack.push(dirName);
+          size = 0;
+        }
+      } else if (command.startsWith('ls')) {
+
+        const contents = output.shift();    
+        contents.forEach(content => {
+          if (!content.startsWith('dir')) {
+            size += Number.parseInt(content.split(" ")[0]);
+          }
+        });
+        const cwd = dirStack.join("/");
+        dirMap.set(cwd, size);
+      }
+    });
+
+    let result = 0;
+    dirMap.forEach((size) => {
+      if (size < 100000) {
+        result += size;
+      }
+    });
+    console.log(result);
+  },
+};
