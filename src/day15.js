2341f53838db2793c08d15433e9f277b8673a343
dwi2 <dwi2@dwi2.com>
2022-12-17 19:20:58 +0900

    fix: Parsing erro of Day 15 and print function

--- src/day15.js
+++ src/day15.js
@@ -4,6 +4,32 @@ const toKey = (x, y) => {
 
 const cmap = new Map();
 
+let leftmost = 0;
+let rightmost = 0;
+let topmost = 0;
+let bottommost = 0;
+
+const print = () => {
+  console.log(leftmost, rightmost, topmost, bottommost);
+  // const xDigit = Math.max(leftmost.toString().length, rightmost.toString().length);
+  const yDigit = Math.max(topmost.toString().length, bottommost.toString().length);
+
+  for(y = topmost; y <= bottommost; y += 1) {
+    const tempY = Array(yDigit).fill(' ').join("") + y.toString() + " ";
+    let line = tempY.substring(tempY.length - yDigit - 1, tempY.length);
+    for (x = leftmost; x <= rightmost; x += 1) {
+      const obj = cmap.get(toKey(x, y));
+      if (obj) {
+       line += obj.type;
+      } else {
+        line += '.';
+      }
+    }
+    console.log(line);
+  }
+  
+};
+
 module.exports = {
   reading: (line) => {
     if (line.length === 0) {
@@ -22,7 +48,7 @@ module.exports = {
       beaconText.match(/closest beacon is at x=(-?[\d]+)/)[1]
     );
     const beaconY = Number.parseInt(
-      beaconText.match(/closest beacon is at x=-?[\d]+, y=(-?[\d])+/)[1]
+      beaconText.match(/closest beacon is at x=-?[\d]+, y=(-?[\d]+)/)[1]
     );
     cmap.set(toKey(sensorX, sensorY), {
       type: "S",
@@ -34,10 +60,35 @@ module.exports = {
         type: "B"
       });
     }
+
+    if (beaconX < leftmost) {
+      leftmost = beaconX;
+    }
+    if (beaconX > rightmost) {
+      rightmost = beaconX;
+    }
+    if (sensorX < leftmost) {
+      leftmost = sensorX;
+    }
+    if (sensorX > rightmost) {
+      rightmost = beaconX;
+    }
+    if (beaconY < topmost) {
+      topmost = beaconY;
+    }
+    if (beaconY > bottommost) {
+      bottommost = beaconY;
+    }
+    if (sensorY < topmost) {
+      topmost = sensorY;
+    }
+    if (sensorY > bottommost) {
+      bottommost = sensorY;
+    }
   },
   solving: () => {
     console.log(cmap);
-
+    print();
 
   },
 };
